d9be83c029d61e157de7f3182530c52c88572906
Mohit Sonwane <101231709+sonwamoh@users.noreply.github.com>
2026-05-17 21:13:35 -0700

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,61 @@
+from collections import defaultdict, deque
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # key: node, val: list [neighbours of the node]
+        # It's bidirectional graph
+        # visited set = set()
+        # number of conected componenets = numbers of time we do bfs on the node
+        # that's not in the visited set
+        # TC: O(n*m), SC: O(n*m), n is num of edges, m is num of vertexes
+
+        adjList = defaultdict(list) 
+
+        for src, dst in edges:
+            adjList[src].append(dst)
+            adjList[dst].append(src)
+        
+        visited = set()
+        q = deque([])
+        cnt = 0
+
+        def bfs(node):
+            q.append(node)
+            visited.add(node)
+
+            # n = 5, edges = [[0,1],[1,2],[3,4]]
+
+            """
+            q = []
+            visited = [0, 1, 2]
+            curr = 2
+            adjList[2] 
+                nei 1
+            """
+
+
+            """
+            2 -> 3, 1
+            3 -> 2, 1
+            1 -> 2, 3
+            """
+
+            while q:
+                curr = q.popleft()
+                for nei in adjList[curr]:
+                    if nei not in visited:
+                        q.append(nei)
+                        visited.add(nei)
+                    else:
+                        continue
+
+
+        for i in range(0, n):
+            if i not in visited:
+                bfs(i)
+                cnt += 1
+        
+        return cnt
+
+
+
+        
