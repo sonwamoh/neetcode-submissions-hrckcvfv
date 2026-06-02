474c0ea65b58804df056518b18d64a15428765b3
Mohit Sonwane <101231709+sonwamoh@users.noreply.github.com>
2026-06-01 20:55:07 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        step 1 : 1 = 1 way
+        step 2: 1 + 1, or 2 way
+        step 3: step 2 way + step 1 way = 2 + 1 = 3 way
+        step 4: step 3 + step 2 = 3 + 2 = 5 way
+        step 5: step 4 + step 3 = 5 + 3 = 8 ways
+        """
+        curr, prev = 2, 1 # confused
+        for i in range(2, n):
+            tmp = curr
+            curr += prev
+            prev = tmp
+        return curr
+        
